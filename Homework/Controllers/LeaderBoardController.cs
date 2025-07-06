bf007ad6a915e04b67fde6b0dcd3b5e1c44d9134
dudu <dudu@MANDY>
2025-07-06 13:11:38 +0800

    添加项目文件。

---
+++ Homework/Controllers/LeaderBoardController.cs
@@ -0,0 +1,94 @@
+﻿using Homework.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Homework.Controllers
+{
+
+    [ApiController]
+    public class LeaderBoardController : ControllerBase
+    {
+        private static readonly SortedSet<Customer> sortedCustomers =
+            new (Comparer<Customer>.Create((x, y) =>
+            { if (y.Score.CompareTo(x.Score) == 0) 
+                    return x.Id.CompareTo(y.Id); 
+                return y.Score.CompareTo(x.Score); }));
+        private static readonly Dictionary<long, Customer> customerMap = [];
+
+        [HttpPost]
+        [Route("/customer/{customerid}/score/{score}")]
+        public ActionResult<int> CreateCustomer(long customerid, [Range(-1000, 1000)] decimal score)
+        {
+            if (!customerMap.TryGetValue(customerid, out Customer? current)) // Use nullable type for 'current'
+            {
+                Customer newCustomer = new (customerid, score);
+                sortedCustomers.Add(newCustomer);
+                customerMap[customerid] = newCustomer;
+            }
+            else
+            {
+                sortedCustomers.Remove(current);
+                current.Score += score;
+                sortedCustomers.Add(current);
+            }
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("/leaderboard")]
+        public ActionResult<IEnumerable<Customer>> GetCustomerByRank(int start,int end)
+        {
+            List <Customer> res = new();
+            SortedSet<Customer>.Enumerator en = sortedCustomers.GetEnumerator();
+            int rank = 0;
+            while (en.MoveNext())
+            {   
+                rank++;
+                if (rank >= start && rank <= end)
+                {
+                    res.Add(en.Current);
+                    en.Current.Rank = rank;
+                }
+                if(rank > end)
+                {
+                    break;
+                }
+            }
+            return Ok(res);
+        }
+
+        [HttpGet]
+        [Route("/leaderboard/{customerid}")]
+        public ActionResult<IEnumerable<Customer>> Details(long customerid,int high,int low)
+        {
+            int range = high;
+            Customer customer = customerMap[customerid];
+            Queue<Customer> queue = new ();
+            SortedSet<Customer>.Enumerator en = sortedCustomers.GetEnumerator();
+            bool foundCustomer = false;
+            int rank = 0;
+            while (en.MoveNext())
+            {
+                rank++;
+                Customer current = en.Current;
+                current.Rank = rank;
+                queue.Enqueue(current);
+                
+                if (current.Id == customerid)
+                {
+                    foundCustomer = true;
+                    range = low;
+                }else range--;
+                if (range < 0&&!foundCustomer)
+                {
+                    queue.Dequeue();
+                    range++;
+                }
+                if(range==0&&foundCustomer) break;
+            }
+
+            return Ok(queue);
+        }
+
+    }
+}
---
+++ Homework/Models/Customer.cs
@@ -0,0 +1,16 @@
+﻿namespace Homework.Models
+{
+    public class Customer
+    {
+        public long Id { get; set; }
+        public decimal Score { get; set; }
+        public int Rank { get; set; }
+
+        public Customer(long id, decimal score)
+        {
+            Id = id;
+            Score = score;
+        }
+       
+    }
+}
